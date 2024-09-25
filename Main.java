22ef16080e206cd48ff8d60a4909126a34b29903
Asadulnyc <asadulk@nycstudents.net>
2024-09-25 15:50:37 +0000

    commit

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main
         //System.out.println(f);
         //System.out.println(f.currentFood);
 
-        Feeder g = new Feeder(500);
-        System.out.println(g.getCurrentFood());
-        g.simulateOneDay(12);
+        //Feeder g = new Feeder(500);
+        //System.out.println(g.getCurrentFood());
+        //g.simulateOneDay(12);
 
         //System.out.println(Math.random());
         //double random = Math.random() * 10;
@@ -23,25 +23,44 @@ public class Main
         //System.out.println(random4);
         //System.out.println(Math.random() < 0.05);
 
-        g.simulateOneDay(12);
-        System.out.println(g.getCurrentFood());
+        //g.simulateOneDay(12);
+        //System.out.println(g.getCurrentFood());
         
-        Feeder h = new Feeder(1000);
-        h.simulateOneDay(22);
-        System.out.println(h.getCurrentFood());
+        //Feeder h = new Feeder(1000);
+        //h.simulateOneDay(22);
+        //System.out.println(h.getCurrentFood());
 
-        Feeder i = new Feeder(100);
-        i.simulateOneDay(5);
-        System.out.println(i);
+        //Feeder i = new Feeder(100);
+        //i.simulateOneDay(5);
+        //System.out.println(i);
 
-        Feeder j = new Feeder(250);
-        System.out.println(j.simulateManyDays(10, 5));
+        //Feeder j = new Feeder(250);
+        //System.out.println(j.simulateManyDays(10, 5));
 
-        Feeder jSecond = new Feeder(0);
-        System.out.println(jSecond.simulateManyDays(5, 10));
+        //Feeder j = new Feeder(0);
+        //System.out.println(j.simulateManyDays(5, 10));
         
-        Feeder k = new Feeder(0);
-        System.out.println(k.simulateManyDays(5, 10));
+        //Feeder k = new Feeder(0);
+        //System.out.println(k.simulateManyDays(5, 10));
+
+        Feeder aTest = new Feeder(500);
+        System.out.println(aTest.simulateOneDay(12));
+
+        Feeder bTest = new Feeder(1000);
+        System.out.println(bTest.simulateOneDay(22));
+
+        Feeder cTest = new Feeder(100);
+        System.out.println(cTest.simulateOneDay(5));
+
+        Feeder dTest = new Feeder(2400);
+        System.out.println(dTest.simulateManyDays(10, 4));
+        
+        Feeder eTest = new Feeder(250);
+        System.out.println(eTest.simulateManyDays(10, 5));
+        
+        Feeder fTest = new Feeder(2400);
+        System.out.println(fTest.simulateManyDays(10, 5));
+
 
     }
 }
